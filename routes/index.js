797721ffc1c190f7bec7db134bd33b7f37783911
Elena Ryzhkova <grishina.elena.p@yandex.ru>
2024-01-09 14:09:38 +0300

    fix central err

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,11 @@ router.use('/cards', cardsRouter); // роутеры для карточек
 // роут для запросов по несуществующим URL
 
 router.use('*', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена. Проверьте правильность ввода URL и метод запроса'));
-});
-
-router.use((err, req, res, next) => {
-  if (err instanceof NotFoundError) {
-    res.status(err.statusCode).json({ error: err.message });
-  } else {
-    next(err);
-  }
+  next(
+    new NotFoundError(
+      'Страница не найдена. Проверьте правильность ввода URL и метод запроса',
+    ),
+  );
 });
 
 module.exports = router;
