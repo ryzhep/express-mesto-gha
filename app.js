ce973da937f01bcca7bf9bc95d956385b58d7653
Elena Ryzhkova <grishina.elena.p@yandex.ru>
2024-01-16 22:28:54 +0300

    14 pr fix

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const router = require('express').Router();
-
 // eslint-disable-next-line no-unused-vars
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const { createUser, login } = require('./controllers/users');
-const {
-  createUserValidator,
-  loginValidator,
-} = require('./middlewares/userValidator');
 
 const appRouter = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
@@ -33,9 +26,6 @@ app.use((req, res, next) => {
 });
 */
 
-// роуты, которые не требуют авторизации (регистрация и логин)
-router.post('/signup', createUserValidator, createUser); // роутер для регистрации
-router.post('/signin', loginValidator, login); // роутер для авторизации
 app.use(appRouter);
 app.use(errors()); // сборка JSON-формата
 app.use(errorHandler); // централизолванная обработка ошибок
